fd56a04a2f0c69a7f5fb1d619dbc7c740816d3e1
bloomanco <bloomanco@gmail.com>
2024-10-09 03:44:11 -0400

    first project changes

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+<html>
+    <head> 
+        <meta name="color-scheme" content="light dark"></meta>
+    </head>
+    <body>
+        <pre style="word-wrap: break-word; white-space: pre-wrap;">alert("Hello World"); let 
+        favoritefood = "Pasta"; document.write(favoritefood);</pre>
+    </body>
+</html>
