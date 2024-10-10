df9709321a6456dcf605214519f8d0df5937b764
bloomanco <bloomanco@gmail.com>
2024-10-10 03:30:52 -0400

    2nd update for task1

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,3 @@
-<html>
-    <head> 
-        <meta name="color-scheme" content="light dark"></meta>
-    </head>
-    <body>
-        <pre style="word-wrap: break-word; white-space: pre-wrap;">alert("Hello World"); let 
-        favoritefood = "Pasta"; document.write(favoritefood);</pre>
-    </body>
-</html>
+alert("Hello World"); 
+let favoritefood = "Pasta"; 
+document.write(favoritefood);
