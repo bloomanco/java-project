bb9318b5a55c3a58976af3b24fb4a24d4965ac0e
bloomanco <bloomanco@gmail.com>
2025-01-12 01:49:35 -0500

    lates pokemon update

---
+++ Archive 2/js/scripts.js
@@ -0,0 +1,41 @@
+let pokemonList = [
+    
+    {
+        id: 0o0011,
+        name: 'Jigglypuff',
+        hp: 115,
+        attack: 45,
+        defense: 20,
+        spAttack: 45,
+        spDefense: 25,
+        speed: 20,
+        height: 0.5,
+        types: ['fairy normal'],
+    
+    },
+    {
+        id: 0o0022,
+        name: 'Butterfree',
+        hp: 60,
+        attack: 45,
+        defense: 50,
+        spAttack: 90,
+        spDefense: 80,
+        speed: 70,
+        height: 1.1,
+        types: ['Bug Flying'],
+        
+    },
+    {
+        id: 0o0033,
+        name:'Raichu',
+        hp: 60,
+        attack: 90,
+        defense: 55,
+        spAttack: 90,
+        spDefense: 80,
+        speed: 110,
+        height: 0.8,
+        types: ['Electric'], 
+    },
+];
--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,28 @@
 let pokemonList = [
+    {
+      id: 111,
+      name: "Jigglypuff",
+      height: 9,
+    },
+    {
+      id: 222,
+      name: "Butterfree",
+      height: 8,
+    },
+    {
+      id: 333,
+      name: "Raichu",
+      height: 5,
+    },
+  ];
+  
+  for (let i = 0; i < pokemonList.length; i++) {
     
-    {
-        id: 0o0011,
-        name: 'Jigglypuff',
-        hp: 115,
-        attack: 45,
-        defense: 20,
-        spAttack: 45,
-        spDefense: 25,
-        speed: 20,
-        height: 0.5,
-        types: ['fairy normal'],
+    let message = `${pokemonList[i].name} (height: ${pokemonList[i].height})`;
     
-    },
-    {
-        id: 0o0022,
-        name: 'Butterfree',
-        hp: 60,
-        attack: 45,
-        defense: 50,
-        spAttack: 90,
-        spDefense: 80,
-        speed: 70,
-        height: 1.1,
-        types: ['Bug Flying'],
-        
-    },
-    {
-        id: 0o0033,
-        name:'Raichu',
-        hp: 60,
-        attack: 90,
-        defense: 55,
-        spAttack: 90,
-        spDefense: 80,
-        speed: 110,
-        height: 0.8,
-        types: ['Electric'], 
-    },
-];
+    if (pokemonList[i].height > 7) {
+      message += " - Wow, that's big!";
+    }
+    
+    document.write(message + "<br>");
+  }
